70af270aaff48b4da2ae0c677e2c676fc19c2945
Krems20 <kremsoup20@gmail.com>
2024-10-16 20:20:58 +0300

    Update download.php

--- download.php
+++ download.php
@@ -2,14 +2,20 @@
 $file = $_GET['file'];
 $filePath = 'files/' . basename($file);
 
+// Проверьте, существует ли файл
 if (file_exists($filePath)) {
+    // Установите заголовки
     header('Content-Description: File Transfer');
     header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename="' . basename($file) . '"');
+    header('Content-Disposition: attachment; filename="' . basename($filePath) . '"');
     header('Expires: 0');
     header('Cache-Control: must-revalidate');
     header('Pragma: public');
     header('Content-Length: ' . filesize($filePath));
+
+    // Очистите буфер вывода перед чтением файла
+    ob_clean();
+    flush();
     readfile($filePath);
     exit;
 } else {
